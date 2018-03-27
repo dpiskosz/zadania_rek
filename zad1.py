4036888bfa0d55befd90e4a262e5245a20631128
dpiskosz <dominika.piskosz@gmail.com>
2018-03-27 15:40:34 +0200

    Zad1 done.

--- zad1.py
+++ zad1.py
@@ -1,1 +1,17 @@
-print("Hello")
+code1 = "79-900"
+code2 = "80-155"
+
+
+def postcode_generator(first_code, last_code):
+    first_code_int = int(first_code.replace("-", ""))
+    last_code_int = int(last_code.replace("-", ""))
+    code_list = []
+    for x in range(first_code_int+1, last_code_int):
+        new_code_str = str(x)
+        new_code = "{}-{}".format(new_code_str[0:2], new_code_str[2:6])
+        code_list.append(new_code)
+    return code_list
+
+
+generate = postcode_generator(code1, code2)
+print(generate)
